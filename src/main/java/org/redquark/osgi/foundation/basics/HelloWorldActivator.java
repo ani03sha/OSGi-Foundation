a4a4c54cd234171772e1f054bfbdeee047d8e265
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-03-16 18:55:18 +0530

    Initial Commit

---
+++ src/main/java/org/redquark/osgi/foundation/basics/HelloWorldActivator.java
@@ -0,0 +1,35 @@
+package org.redquark.osgi.foundation.basics;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+public class HelloWorldActivator implements BundleActivator {
+
+	// Logger to log events - SLF4J logger
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * This method is called when the bundle is started. All the initialization
+	 * tasks should be performed here.
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		log.info("Hello, World!");
+	}
+
+	/**
+	 * This method is called when the bundle is stopped. All the clean up tasks
+	 * should be performed here.
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		log.info("Goodbye, World!");
+	}
+
+}
